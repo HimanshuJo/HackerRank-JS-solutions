a5a1059a5516b7c64e5d79c7a67055beaadb93f3
Himanshu Joshi <himanshhjoshi@outlook.com>
2024-02-10 11:24:58 +0100

    upd

---
+++ UtopianTree.js
@@ -0,0 +1,61 @@
+/*
+	https://www.hackerrank.com/challenges/utopian-tree/problem?isFullScreen=true
+*/
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'utopianTree' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function utopianTree(n) {
+    if(n==0) return 1;
+    let res=1, count=0;
+    while(n--){
+        if(count%2==0) res*=2;
+        else res++;
+        count++;
+    }
+    return res;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const n = parseInt(readLine().trim(), 10);
+
+        const result = utopianTree(n);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
